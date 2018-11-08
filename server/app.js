92d274b424a772e111f245610a89ba7402f460a4
Vignesh Balasundaram <vigneshbalasundaram@gmail.com>
2018-11-08 12:09:35 -0800

    Update CSS

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 const axios = require('axios');
 
 const app = express();
+
+app.use(morgan('dev'));
 
 app.use(express.static(path.join(__dirname, './../public')));
 app.use(express.static(path.join(__dirname, './../node_modules')));
